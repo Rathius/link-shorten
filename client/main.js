138c2cd0b12e5d2e5d6e9ef2bb4a8dfce6277fc2
Rathius <rathius@icloud.com>
2016-10-26 04:47:52 -0400

    added WebApp and connect-route to project and finished the redirect logic

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Header from './components/header.js';
 import LinkCreate from './components/link-create';
 import { Links } from '../imports/collections/links';
+import LinkList from './components/link-list';
 
 const App = () => {
 	return (
@@ -11,6 +12,7 @@ const App = () => {
 			<Header />
 			<div className="container">
 				<LinkCreate />
+				<LinkList />
 			</div>
 		</div>
 	);
